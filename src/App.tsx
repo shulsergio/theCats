7183d58bedace54428bdce772b33cf4ae5de9954
Sergey Shulga <shul.sergio@gmail.com>
2024-10-10 17:08:30 +0300

    v 1.0 start

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+// import Search from './components/Search/Search'
+
+function App() {
+
+
+    // const onHandleSubmit = (): void => {
+    // setQuery(searchQuery);
+    // setPhotos([]);
+    // setPage(1);
+    // setError(null);
+    // setVisible(false);
+    // setEmpty(false);
+  };
+  // const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <Search onSubmit={ onHandleSubmit} />
+//     </>
+//   )
+// }
+
+export default App
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,17 @@
+
+
+// interface SearchProps{
+//     onSubmit: () => void;
+// }
+
+// const Search = ({ onSubmit}:SearchProps) => { 
+
+//     return (
+//         <div>
+// <Button>
+//         </div>
+//     )
+
+
+// }
+// export default Search;
